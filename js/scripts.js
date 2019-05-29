393467858cf1b76e92af2f3f1bd23959f646c2e0
Ian Alexander <iralex255@gmail.com>
2019-05-28 17:25:38 -0700

    it will add questions and effects

--- js/scripts.js
+++ js/scripts.js
@@ -7,17 +7,23 @@ $(document).ready(function() {
 $(document).ready(function() {
   $(".fly").click(function() {
     $(".second-clue").show();
+    $(".first-clue").remove();
   });
 });
 
 $(document).ready(function() {
   $(".one").click(function() {
     $(".third-clue").show();
+    $(".second-clue").remove();
   });
 });
 
 $(document).ready(function() {
-  $(".now").click(function() {
-    prompt ("nice. I feel like you're getting to know me pretty well by now. Can I know about you? What is your name?");
+  $("#click-cat").click(function() {
+    var name = prompt("nice. you're doing great! But I feel like you're getting to know me, but I still don't know you. What is your name?");
+    alert(name + "!");
+    alert("I need your help, " + name + "!")
+    var answer = prompt("I'm stuck inside this computer! I've been in here for years waiting for you, " + name +". You are my only hope!! The only way out is to solve this riddle. Are you ready?")
+    alert(answer + " is the answer I was looking for. Here we go...")
   });
 });
